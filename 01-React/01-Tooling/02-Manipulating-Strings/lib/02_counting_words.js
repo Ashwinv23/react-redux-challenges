4d1f2cfa02e8ed77a32a1da2c1c69175aca5f23b
Ashwin Vijendra <ack4and@gmail.com>
2019-05-12 15:48:50 +0530

    solved dash and occurrences challenge

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,16 @@
 function occurrences(text, word) {
-  return null;
+  const gotText = text.toLowerCase();
+  const gotWord = word.toLowerCase();
+  const textArray = gotText.split(' ');
+  let counterForWord = 0;
+
+  textArray.forEach((elem) => {
+    if (elem === gotWord) {
+      counterForWord += 1;
+    }
+  });
+  return counterForWord;
 }
 
+console.log(occurrences("this is so Super Super cool", "super"));
 module.exports = occurrences;
